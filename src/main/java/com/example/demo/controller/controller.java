3932cd47a7befec83db42f83da0c14f27f17390c
Sagar kalia <94918035+Sagar9178@users.noreply.github.com>
2025-02-05 00:07:47 +0530

    Update controller.java

--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 	@GetMapping("/")
 	public String ok() {
-		return "Welcome to Ashok It...!!";
+		return "Welcome to Sagar kalia...!!";
 	}
 }
